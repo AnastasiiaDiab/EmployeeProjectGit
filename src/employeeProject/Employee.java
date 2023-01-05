4e8b40d73c914967386e9fa754cea75ea88c491f
lyaas <lyaastud@gmail.com>
2023-01-05 10:58:17 -0500

    Added ParentClass Employee

---
+++ src/employeeProject/Employee.java
@@ -0,0 +1,17 @@
+package employeeProject;
+
+import java.time.LocalDate;
+
+public abstract class Employee {
+
+    private String name;
+    private int age;
+    private LocalDate DOB;
+    private String jobTitle, employeeId;
+    private double salary;
+
+    public abstract void work();
+
+
+
+}
